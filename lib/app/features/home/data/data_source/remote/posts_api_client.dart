19bcc7d1701854574a41f33e3b2b64abd7c054e9
Henry Ifebunandu <henryifebunandu@gmail.com>
2023-03-31 04:50:29 +0100

    fix: remove unnecessary code

--- lib/app/features/home/data/data_source/remote/posts_api_client.dart
+++ lib/app/features/home/data/data_source/remote/posts_api_client.dart
@@ -1,11 +1,5 @@
-import 'package:http/http.dart';
-
 import '../../../../../shared/models/posts_response.dart';
 
 abstract class PostsApiClient {
-  PostsApiClient({required Client client}) : _client = client;
-
-  final Client _client;
-
   Future<PostsResponse> getPosts();
 }
